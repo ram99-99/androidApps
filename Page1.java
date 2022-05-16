275127f52f43407946bd2805fc3b0996f58e5df1
ram99-99 <85692825+ram99-99@users.noreply.github.com>
2022-05-16 22:34:07 +0530

    fragmentpageadpater

---
+++ Page1.java
@@ -0,0 +1,27 @@
+package com.example.pageview;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+public class Page1 extends Fragment {
+
+    public Page1() {
+        // required empty public constructor.
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_page1, container, false);
+    }
+}
