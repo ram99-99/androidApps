f6a26d55a3781dd7b599b77251ff8cc3d597575e
ram99-99 <85692825+ram99-99@users.noreply.github.com>
2022-05-17 21:10:14 +0530

    Add files via upload

---
+++ pageView/app/src/main/java/com/example/pageview/Page1.java
@@ -0,0 +1,28 @@
+package com.example.pageview;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+public class Page1 extends Fragment {
+
+// required empty public constructor.
+    public Page1() {
+        
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_page1, container, false);
+    }
+}
